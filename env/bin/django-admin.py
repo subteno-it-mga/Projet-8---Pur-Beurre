5697162a5ab97d23f2fe343703a4dfbef0ef131f
Martin Gaucher <martin.gaucher@subteno-it.fr>
2019-11-19 11:45:47 +0100

    [FIX] rm env

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/martingaucher/Documents/OpenClassrooms/Projet/Projet 8 - Pur Beurre/Projet-8---Pur-Beurre/env/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
