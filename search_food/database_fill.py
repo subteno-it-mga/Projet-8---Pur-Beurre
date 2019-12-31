9d9056a87068c24c26f00b86efe9592d4eaf254b
Martin Gaucher <mga@subteno-it.com>
2019-12-31 10:30:22 +0100

    [ADD] Custom command filling the database

---
+++ search_food/database_fill.py
@@ -0,0 +1,50 @@
+import json
+from urllib.request import urlopen
+from .models import Product
+
+
+def get_files():
+    '''
+    Get the informations from the openfoodfact API.
+    '''
+    product = 'nutella'
+    url = "https://world.openfoodfacts.org/cgi/search.pl?search_terms=%s&action=process&json=1&page_size=10" % (product)
+    result = urlopen(url)
+    json_result = json.load(result)
+    product_dict = json_result["products"]
+
+    return product_dict
+
+def pre_fill_database():
+    '''
+    We fill the database with the informations obtained from get_files function.
+    '''
+    product = get_files()
+    try:
+        description = product[0]["generic_name"]
+        name = product[0]["product_name"]
+        salt = product[0]["nutriments"]["salt"]
+        sugar = product[0]["nutriments"]["sugars"]
+        fat = product[0]["nutriments"]["fat"]
+        nutriscore = product[0]["nutrition_grades"]
+        barcode = product[0]["code"]
+        image = product[0]["image_front_url"]
+        category = product[0]["compared_to_category"]
+
+        Product.objects.create(
+            description=description,
+            name=name,
+            salt=salt,
+            sugar=sugar,
+            fat=fat,
+            nutriscore=nutriscore,
+            barcode=barcode,
+            image=image,
+            category=category,
+            search='nutella'
+        )
+        print("Product is in database.")
+
+    except KeyError:
+
+        print("Can't access to the data.")
---
+++ search_food/management/commands/fill_database.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from search_food.database_fill import pre_fill_database
+
+class Command(BaseCommand):
+    help = 'Pre fill the database'
+
+    def handle(self, *args, **options):
+        self.stdout.write('Filling the database...')
+        try:
+            pre_fill_database()
+            self.stdout.write('Database filled.')
+        except:
+            self.stdout.write('There was a problem during the process')
